e0f6a02681ab5f89d8bc29899e6a01e11ab6d076
DevasishY <48542948+DevasishY@users.noreply.github.com>
2022-09-05 16:53:39 +0530

    Create Square of a Sorted Array.cpp

---
+++ Arrays/Square of a Sorted Array.cpp
@@ -0,0 +1,53 @@
+Approch : 1
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        for(int i=0;i<nums.size();i++)
+        {
+            nums[i]=nums[i]*nums[i];
+        }
+       sort(nums.begin(),nums.end());
+       return nums;
+        
+    }
+};
+/*
+TC :O(NlogN)
+SC :O(1)
+*/
+
+Approch :2
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) 
+    {
+        vector<int> v;
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r)
+        { 
+          int smallvalue = nums[l];
+          int largervalue = nums[r];
+          
+          if(abs(smallvalue) >abs(largervalue))
+          {
+              v.push_back(smallvalue*smallvalue);
+              l++;
+          }
+            else
+            {
+              v.push_back(largervalue*largervalue);
+              r--; 
+            }
+        }
+        reverse(v.begin(),v.end());
+        return v;
+    }
+};
+
+  
+/*
+TC:O(N)
+SC:O(N)
+*/
